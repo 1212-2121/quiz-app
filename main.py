8e95e02fd72f107b4faf379ab6de0bcad9ee8367
1212-2121 <kidistdereje084@gmail.com>
2025-02-14 23:48:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,12 @@ def read_questions(file_name):
     with open(file_name,'r') as file:
         for line in file:
             parts=line.strip().split('|')
+            if parts<3:
+                continue
+            question=parts[o]
+            correct_answer=parts[1].lower()
+            options=parts[2:]
+            questions.append(question,correct_answer,options)
 
 
+    return questions
